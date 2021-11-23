5201179bd6f8c988706bbc84a5b13aa4c4ac51c9
Juan Carranza <ikkicarranza@gmail.com>
2021-11-22 21:38:21 -0400

    Proyecto Creado

---
+++ src/main/java/bo/edu/ucb/Sakila/Videoclub/SakilaVideoClubApplication.java
@@ -0,0 +1,14 @@
+package bo.edu.ucb.Sakila.Videoclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakilaVideoClubApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SakilaVideoClubApplication.class, args);
+	}
+
+}
