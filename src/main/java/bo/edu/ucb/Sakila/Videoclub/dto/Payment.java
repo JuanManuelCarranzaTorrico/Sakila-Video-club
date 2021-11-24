3a7c7b6ae2979d74663539cc903e24e175d04b50
Juan Carranza <ikkicarranza@gmail.com>
2021-11-24 00:27:25 -0400

    Alpha 1.0.2

---
+++ src/main/java/bo/edu/ucb/Sakila/Videoclub/dto/Payment.java
@@ -0,0 +1,56 @@
+package bo.edu.ucb.Sakila.Videoclub.dto;
+import java.util.Date;
+public class Payment {
+    private Integer paymentId;
+    private Integer customerId;
+    private Integer staffId;
+    private Integer rentalId;
+    private double amount;
+    private Date paymentDate;
+    private Date lastUpDate;
+    public Payment(){
+        
+    }
+    public Date getLastUpDate() {
+        return lastUpDate;
+    }
+    public Integer getPaymentId() {
+        return paymentId;
+    }
+    public void setPaymentId(Integer paymentId) {
+        this.paymentId = paymentId;
+    }
+    public Integer getCustomerId() {
+        return customerId;
+    }
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+    public Integer getStaffId() {
+        return staffId;
+    }
+    public void setStaffId(Integer staffId) {
+        this.staffId = staffId;
+    }
+    public Integer getRentalId() {
+        return rentalId;
+    }
+    public void setRentalId(Integer rentalId) {
+        this.rentalId = rentalId;
+    }
+    public double getAmount() {
+        return amount;
+    }
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+    public Date getPaymentDate() {
+        return paymentDate;
+    }
+    public void setPaymentDate(Date paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+    public void setLastUpDate(Date lastUpDate) {
+        this.lastUpDate = lastUpDate;
+    }
+}
---
+++ src/main/java/bo/edu/ucb/Sakila/Videoclub/dto/Rental.java
@@ -0,0 +1,63 @@
+package bo.edu.ucb.Sakila.Videoclub.dto;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+public class Rental {
+    private Integer rentalId;
+    private Date rentalDate;
+    private Integer inventoryId;
+    private Integer customerId;
+    private Date returnDate;
+    private Integer staffId;
+    private Date lastUpdate;
+
+    public Rental(){
+        
+    }
+
+    public Integer getRentalId() {
+        return rentalId;
+    }
+    public Date getLastUpdate() {
+        return lastUpdate;
+    }
+    public void setLastUpdate(Date lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+    public Integer getStaffId() {
+        return staffId;
+    }
+    public void setStaffId(Integer staffId) {
+        this.staffId = staffId;
+    }
+    public Date getReturnDate() {
+        return returnDate;
+    }
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+    public Integer getCustomerId() {
+        return customerId;
+    }
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+    public Integer getInventoryId() {
+        return inventoryId;
+    }
+    public void setInventoryId(Integer inventoryId) {
+        this.inventoryId = inventoryId;
+    }
+    public Date getRentalDate() {
+        return rentalDate;
+    }
+    public void setRentalDate(Date rentalDate) {
+        this.rentalDate = rentalDate;
+    }
+    public void setRentalId(Integer rentalId) {
+        this.rentalId = rentalId;
+    }
+    
+   
+}
